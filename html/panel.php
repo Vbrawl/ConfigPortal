fc1f8157715d2ce47729448cd3ea1ac307a421f1
Vbrawl <greninjagr@gmail.com>
2022-09-02 21:45:05 +0300

    Added some meta settings and linked to files.

--- html/panel.php
+++ html/panel.php
@@ -2,9 +2,12 @@
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, height=device-height, initial-scale=1.0, maximum-scale=1.0">
     <title>Config Panel</title>
+    <link rel="stylesheet" href="/css/panel.css">
+    <script src="/js/panel.js" charset="utf-8"></script>
   </head>
   <body>
-    
+    <div id="dynamic"></div>
   </body>
 </html>
