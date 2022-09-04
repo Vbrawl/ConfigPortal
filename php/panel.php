8dbe32c47ee129c49d87d355db442cfd924b2293
Vbrawl <greninjagr@gmail.com>
2022-09-04 20:22:49 +0300

    Added support for multiple files.

--- php/panel.php
+++ php/panel.php
@@ -2,20 +2,20 @@
 
 session_start();
 
-if(!isset($_SESSION["LOGGED_IN"]) || !isset($_GET["action"])) {exit(1);}
+if(!isset($_SESSION["LOGGED_IN"]) || !isset($_GET["action"]) || !isset($_GET["file"])) {exit(1);}
 
 include "config.php";
 
 if($_SESSION["LOGGED_IN"] == true) {
 
   if($_GET["action"] == "read_config") {
-    readfile($config_path);
+    readfile($config_paths[(int)$_GET["file"]]);
     exit(0);
   }
 
 
   else if($_GET["action"] == "write_config") {
-    $file = fopen($config_path, 'w');
+    $file = fopen($config_paths[(int)$_GET["file"]], 'w');
     fwrite($file, file_get_contents("php://input"));
     fclose($file);
     exit(0);
