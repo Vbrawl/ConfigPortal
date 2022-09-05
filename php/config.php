c3b65297a78f1a1024b1aae5bc493d6bc32488e3
Vbrawl <greninjagr@gmail.com>
2022-09-05 22:45:14 +0300

    Added program list.

--- php/config.php
+++ php/config.php
@@ -3,8 +3,13 @@
 
 $password = "test";
 
-$config_paths = ["config.conf", "config2.conf"]
+$config_paths = ["config.conf", "config2.conf"];
 
 
+
+$program_names = ["test.py"];
+$start_program_lines = ["python ../test.py"];
+$stop_program_lines = ["python process_actions.py stop test.py"];
+$program_status_lines = ["python process_actions.py check test.py"];
 
  ?>
